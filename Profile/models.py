ed013b060967fb633d2c2652cb05b4e621093ca4
Nirmal <nirmalpandey048@gmail.com>
2020-05-17 15:35:57 +0545

    password_reset added

--- Profile/models.py
+++ Profile/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+
+
+
+
+
+
+
+
+
+
+
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from Profile import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -25,6 +26,20 @@ urlpatterns = [
     path('',include('Profile.urls',namespace="profile")),
     path('',include('store.urls',namespace='store')),
     path('',include('store.category_urls',namespace="category_app")),
+    
+      path('reset_password/',auth_views.PasswordResetView.as_view(template_name = 'passwordreset/password_reset_email.html'), 
+         name = "password_reset"),
+    
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name = 'passwordreset/password_reset_sent.html'), 
+         name = "password_reset_done"),
+    
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='passwordreset/password_reset_form.html'),
+         name="password_reset_confirm"),  
+       
+     #<token> check  for valid user or not--><uidb64> user id encoded in base 64--this email is sent to the user
+     #<uidb64> helps to know user who request for password
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name='passwordreset/password_reset_complete.html'),
+         name="password_reset_complete"),
   
     
 ]
