b1247673f00327be37f1f4d87611d6dcc78baff5
Nirmal <nirmalpandey048@gmail.com>
2020-07-21 10:06:42 +0545

    get 404 error so change default to true

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -15,4 +15,5 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
+
 application = WhiteNoise(application)
