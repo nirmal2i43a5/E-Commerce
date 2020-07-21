0e4a0dc2f85204a738a2afde416745b74284bca8
Nirmal <nirmalpandey048@gmail.com>
2020-07-21 09:57:57 +0545

    Updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default = True, cast=bool)
+DEBUG = config('DEBUG', default = False, cast=bool)
 
 ALLOWED_HOSTS = ['nirmalecommerce.herokuapp.com/','127.0.0.1']
 
@@ -140,7 +140,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 # EMAIL_PORT = config('EMAIL_PORT', cast=int)
 
