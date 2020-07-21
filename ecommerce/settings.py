53b437bcdd94193132f886f027436b3cd5ece009
Nirmal <nirmalpandey048@gmail.com>
2020-07-21 09:38:27 +0545

    Updated setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -120,7 +120,7 @@ LOGOUT_REDIRECT_URL = '/login'
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')#for heroku staticfiles
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#for heroku staticfiles
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
