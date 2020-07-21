118f8f01952b470eeb2cb1620d96b37768313159
Nirmal <nirmalpandey048@gmail.com>
2020-07-21 09:48:40 +0545

    Updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://nirmalecommerce.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
