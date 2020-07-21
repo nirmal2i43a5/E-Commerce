9a8fdffd1bf1a042e009e597197c2f604e5882ee
Nirmal <nirmalpandey048@gmail.com>
2020-07-21 10:02:30 +0545

    Updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,7 +145,7 @@ AUTHENTICATION_BACKENDS = (
 # EMAIL_PORT = config('EMAIL_PORT', cast=int)
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = config('EMAIL_PORT', cast = int)
+EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')#this is the jjpassword that i use in gmail 
