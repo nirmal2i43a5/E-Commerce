958530ac068bc5384c26fa84de09061611d9d753
Nirmal <nirmalpandey048@gmail.com>
2020-07-21 10:04:50 +0545

    Updated

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
