68d26b19b89ce939d675c1f37cd95593d8741e6d
Nirmal <nirmalpandey048@gmail.com>
2020-11-14 14:10:04 +0545

    Updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,7 +1,7 @@
 
 
 import os
-from decouple import config,Csv
+from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -72,18 +72,19 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
-#using above is for heroku without that also postgres assists it directly but if sqlite is not using then it doesnot support in localhost
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
+#using above is for heroku without that also postgres assists it directly but if sqlite is not using then it doesnot support in localhost
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -130,6 +131,7 @@ STATIC_URL = '/static/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#for heroku staticfiles
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
